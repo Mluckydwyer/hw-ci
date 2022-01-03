ec7ee1840f09f0f94392c870bdaf7dd20b0c8aa3
Matthew Dwyer <mluckydwyer@gmail.com>
2022-01-03 13:54:38 -0600

    Removed examples

--- hw/sgp_viewPort/tb/run-tests.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-LOG_DIR="../logs"
-FILE_NAME=$(date +"%b-%d-%y_%H-%M-%S")
-TB_NAME="Baisc Axis Adder"
-echo "\e[32m--- Running $TB_NAME Tests: Logging to $LOG_DIR/$FILE_NAME_results.log ---\e[0m"
-cocotb-clean -r
-pytest -n auto --forked --num-tests 5 --reruns 3 --reruns-delay 3 --new-first --cache-clear --self-contained-html --html=$LOG_DIR/$FILE_NAME_results.html --log-file=$LOG_DIR/$FILE_NAME_results.log
-# --num-tests 5
--- resources/modelsim-gui.bat
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-docker run -it -e DISPLAY=host.docker.internal:0.0 -e LIBGL_ALWAYS_INDRIECT=1 mluckydwyer/hw-ci:latest bash
